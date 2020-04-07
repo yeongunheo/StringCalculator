a26daec417b00f3eb0b37f742530ff5b787c8555
yeongunheo <52141636+yeongunheo@users.noreply.github.com>
2020-04-07 23:16:12 +0900

    Update StringCalculator.java
    두번째 StringCalculator

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -6,39 +6,53 @@ import java.util.regex.Pattern;
 public class StringCalculator {
 
 	public int add(String text) {
-		if (text == null || text.isEmpty() || text == " ") return 0;
-		if (text.charAt(0) == '-') {
-			throw new RuntimeException();
-		}
+		if (text == null || text.isEmpty()) return 0;
+		
+		String[] str = split(text);
 		
 		int number = 0;
-		try {
-			number = Integer.parseInt(text);		
-			return number;
-		}catch(NumberFormatException e) {
-			
-			try {
-				String str[] = text.split(",|:");
-				for (int i = 0; i < str.length; i++) {
-					number += Integer.parseInt(str[i]);
-				}	
-			}catch(NumberFormatException ex) {
-				Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
-				if(m.find()) {
-					String customDelimeter = m.group(1);
-					String tokens[] = m.group(2).split(customDelimeter);
-				
-					for (int i = 0; i < tokens.length; i++) {
-						number += Integer.parseInt(tokens[i]);
-					}	
-					
-					return number;
-				}
-			}
-			
-			return number;
+		number = sum(str);
+		
+		return number;
+	}
+	
+	public String[] split(String text) {
+		String[] str = text.split(",|:");
+		if (text.charAt(0) == '/') str = custiomDelimeter(text);
+		
+		return str;
+	}
+	
+	public int sum(String[] str) {
+		int number = 0;
+		for (int i = 0; i < str.length; i++) {
+			negativeException(str[i]);
+			number += Integer.parseInt(str[i]);
+		}	
+		return number;
+	}
+	
+	public String[] custiomDelimeter(String text) {
+		Pattern p = Pattern.compile("//(.)\n(.*)");
+		Matcher m = p.matcher(text);
+		
+		String customDelimeter;
+		String[] str = null;
+		if (m.find()) {
+			customDelimeter = m.group(1);
+			str = m.group(2).split(customDelimeter);
 		}
 		
+		return str;
 	}
-
+	
+	// 반복문과 조건문을 동시에 사용하지 않고
+	// 즉, 인덴트의 길이를 1로 유지하면서 음수 전달 예외처리를 어떻게 할 것인가
+	// 메소드화한다.
+	public void negativeException(String str) {
+		if (Integer.parseInt(str) < 0) {
+			throw new RuntimeException();
+		}
+	}
+	
 }
